4f4f37c483b3cc6a6927fe67056eaf85fdb97aa4
Faeze Rahmati <rahmati.faeze1997@gmail.com>
2026-07-09 16:59:19 +0330

    refactor(dashboard): remove redundant styles and markup
    - Replace .dashboard-item with :host in multiple components
    - Remove unnecessary div wrappers in HTML templates
    - Update component encapsulation and host settings
    - Adjust styles to use :host selector for better consistency

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -4,7 +4,7 @@ import { DashboardItemComponent } from '../dashboard-item/dashboard-item.compone
 @Component({
   selector: 'app-server-status',
   standalone: true,
-  imports: [DashboardItemComponent],
+  imports: [],
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
