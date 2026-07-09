8f84d4774525aaf89f289196745ab209b808fb8a
Faeze Rahmati <rahmati.faeze1997@gmail.com>
2026-07-09 23:28:20 +0330

    feat(server-status): implement dynamic server status simulation
    - Update CSS to use more expressive color codes for online and offline statuses
    - Implement automatic status change every 5 seconds
    - Introduce randomization to simulate different server statuses (online, offline, unknown)

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent {
-  currentStatus = 'unknown';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  constructor() {
+    setInterval(() => {
+      const randomValue = Math.random();
+      if (randomValue < 0.5) {
+        this.currentStatus = 'online';
+      } else if (randomValue < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
